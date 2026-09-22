68ef0fd9c79136ad2c288aff605f17adde150289
Vinicius <viniciusabreu46311@hotmail.com>
2026-09-22 09:18:10 -0300

    Crud básico

--- src/server.js
+++ src/server.js
@@ -25,6 +25,43 @@ app.post("/tarefas", async (req, res) => {
     res.status(201).json(tarefa);
 });
 
+app.get("/tarefas/:id", async (req, res) => {
+    const id = Number(req.params.id);
+
+    const tarefa = await prisma.tarefa.findUnique({
+        where: {
+            id: id
+        }
+    });
+
+    res.json(tarefa);
+});
+
+app.put("/tarefas/:id", async (req, res) => {
+    const id = Number(req.params.id);
+
+    const tarefa = await prisma.tarefa.update({
+        where: {
+            id: id
+        },
+        data: req.body
+    });
+
+    res.json(tarefa);
+});
+
+app.delete("/tarefas/:id", async (req, res) => {
+    const id = Number(req.params.id);
+
+    await prisma.tarefa.delete({
+        where: {
+            id: id
+        }
+    });
+
+    res.status(204).send();
+});
+
 app.listen(3000, () => {
     console.log("Servidor rodando em http://localhost:3000");
 });
